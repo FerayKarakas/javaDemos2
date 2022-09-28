886868d339c825f806ddfc573f3589b9651e2c1d
FerayKarakas <feraykarakas06@gmail.com>
2022-09-28 09:53:33 +0300

    Java ikinci ödev eklendi

---
+++ reCapDemo_Classes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+    DortIslem dortIslem = new DortIslem();
+    int sonuc1 = dortIslem.Topla(3,4);
+    System.out.println(sonuc1);
+
+    int sonuc2 = dortIslem.Cikar(5,2);
+    System.out.println(sonuc2);
+
+    int sonuc3 = dortIslem.Carp(6,8);
+    System.out.println(sonuc3);
+
+    int sonuc4 = dortIslem.Bol(4,2);
+    System.out.println(sonuc4);
+    }
+}
